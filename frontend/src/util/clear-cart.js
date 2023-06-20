8285e7e5a53213d5c9f43fa8fc94bec1c1de04bd
Mert Evirgen <130386211+Rhayaden@users.noreply.github.com>
2023-06-20 15:42:17 +0300

    Delete frontend directory

--- frontend/src/util/clear-cart.js
+++
@@ -1,9 +0,0 @@
-const token = localStorage.getItem("token");
-export const clearCart = async () => {
-  const res = await fetch("http://localhost:8080/cart/clear-cart", {
-    headers: {
-      Authorization: "Bearer " + token,
-    },
-  });
-  return res.json();
-};
